73b1b22c84fbfa5028456494c4cd6f4ad13a85a1
Serg0207 <shprotto26@gmail.com>
2025-07-14 00:44:39 +0300

    Update fetch.py

--- fetch.py
+++ fetch.py
@@ -1,19 +1,14 @@
-
 import requests
-import os
 
 url = "https://docs.ozon.ru/api/seller/swagger.json"
-headers = {
-    "User-Agent": "Mozilla/5.0",
-    "Referer": "https://docs.ozon.ru/api/seller/"
-}
+output_path = "docs/swagger.json"
 
-r = requests.get(url, headers=headers)
+response = requests.get(url, headers={
+    "User-Agent": "Mozilla/5.0"
+})
+response.raise_for_status()
 
-if r.status_code == 200:
-    os.makedirs("docs", exist_ok=True)
-    with open("docs/swagger.json", "w", encoding="utf-8") as f:
-        f.write(r.text)
-    print("✅ swagger.json успешно обновлён.")
-else:
-    print(f"❌ Ошибка {r.status_code}: не удалось получить swagger.json")
+with open(output_path, "w", encoding="utf-8") as f:
+    f.write(response.text)
+
+print(f"Файл сохранён в {output_path}")
